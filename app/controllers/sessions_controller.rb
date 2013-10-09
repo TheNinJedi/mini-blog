76b5077ff266bd4ced349cfe335cfe945a05b3c2
TheNinJedi <TheNinJedi@gmail.com>
2013-10-09 16:12:21 -0600

    Completes sessions and authentication code.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    author = User.find_by_email(params[:email])
+    author = Author.find_by_email(params[:email])
     if author && author.authenticate(params[:password])
       session[:author_id] = author.id
       #cookies[:author_id] = author.id
       redirect_to root_url, notice: "You Are Now Signed In!"
+      # flash.now.notice = "Signed In!"
+      # redirect_to root_url
     else 
       flash.now.alert = "Email or password is invalid!"
       render :new
     end
+  end
+
+  def destroy
+    session[:author_id] = nil
+    redirect_to root_url, notice: "You Are Now Signed Out"
   end
 end
 
